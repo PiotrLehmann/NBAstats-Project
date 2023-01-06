989a7e01a18c5cc4b50b63f50f7a4cf64a7941c5
PiotrLehmann <piterrubik@gmail.com>
2023-01-06 15:38:00 +0100

    changed to a selenium

--- src/PlayerStats.java
+++ src/PlayerStats.java
@@ -1,20 +1,11 @@
-import org.apache.log4j.Logger;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
 import javax.swing.*;
-import javax.swing.table.DefaultTableModel;
 import java.awt.*;
-import java.time.Duration;
+import java.util.List;
 
 public class PlayerStats {
 
@@ -25,30 +16,26 @@ public class PlayerStats {
 
         final String url = "https://www.nba.com/stats/players/traditional";
 
-        //ChromeOptions options = new ChromeOptions();
-        //options.setHeadless(true);
         WebDriver driver = new ChromeDriver();
         driver.get(url);
-        Document document = Jsoup.parse(driver.getPageSource());
 
         String[][] data = new String[50][6];
 
-        try {
-            int rowNumber = 0;
-            for (Element row : document.select("tbody.Crom_body__UYOcU tr")) {
+        WebElement table = driver.findElement((By.xpath("//table[contains(@class, 'Crom_table__p1iZz')]//tbody")));
 
-                final String playerName = row.select(".Crom_stickySecondColumn__29Dwf.Crom_primary__EajZu.Crom_text__NpR1_").text();
-                final String teamName = row.select("td.Crom_text__NpR1_:nth-of-type(3)").text();
-                final String points = row.select("td:nth-of-type(9)").text();
-                final String fieldGoalPercentage = row.select("td:nth-of-type(12)").text();
-                final String assists = row.select("td:nth-of-type(22)").text();
-                final String rebounds = row.select("td:nth-of-type(21)").text();
+        List<WebElement> rows = table.findElements(By.tagName("tr"));
 
-                data[rowNumber] = new String[] {playerName, teamName, points, fieldGoalPercentage, assists, rebounds};
-                rowNumber ++;
+        int numberOfRow = 0;
+        for (WebElement row: rows) {
+            int numberOfColumn = 0;
+            List<WebElement> cols = row.findElements(By.xpath("td"));
+            for (WebElement col : cols) {
+                switch (numberOfColumn) {
+                    case 1, 2, 3, 4, 5, 6 -> data[numberOfRow][numberOfColumn - 1] = col.getText();
+                }
+                numberOfColumn++;
             }
-        } catch (Exception e) {
-            e.printStackTrace();
+            numberOfRow++;
         }
 
         Object[] columns = {"Name", "Team", "Points", "FG%", "Assists", "Rebounds"};
