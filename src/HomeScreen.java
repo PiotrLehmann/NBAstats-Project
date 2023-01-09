931aea27f22aa3ee9189cd2a333c0a13d3e54117
PiotrLehmann <piterrubik@gmail.com>
2023-01-09 21:47:42 +0100

    days till end added

--- src/HomeScreen.java
+++ src/HomeScreen.java
@@ -3,6 +3,7 @@ import java.time.LocalDate;
 import javax.swing.*;
 import java.awt.*;
 import java.text.SimpleDateFormat;
+import java.sql.Date;
 import java.time.Month;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
@@ -16,8 +17,8 @@ public class HomeScreen {
     public JLabel dateLabel;
     public JLabel welcomeLabel;
     public JLabel descriptionLabel;
-    public LocalDate today;
-    public LocalDate endOfSeason;
+    public int today;
+    public int endOfSeason;
     public SimpleDateFormat dateFormat;
     public String date;
     private static final Logger logger = Logger.getLogger(HomeScreen.class);
@@ -31,32 +32,37 @@ public class HomeScreen {
 
         // Info for the right side of HomeScreen
         welcomeLabel = new JLabel();
+        welcomeLabel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
         welcomeLabel.setFont(myFont);
         welcomeLabel.setText("""
                 <html>WELCOME TO THE<br>
                 NEW NBA-STATS APP</br></html>""");
 
         descriptionLabel = new JLabel();
+        descriptionLabel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
         descriptionLabel.setFont(new Font("Arial", Font.PLAIN, 15));
         descriptionLabel.setText("""
-                <html>Check your favourite team record,<br>
-                look at the best players stats,</br><br>
+                <html>Check your favourite team record<br>
+                look at the best players stats</br><br>
                 follow their ranking and more!</br></html>""");
 
         dateFormat = new SimpleDateFormat("E dd.MM.yy");
         dateLabel = new JLabel();
-        dateLabel.setFont(myFont);
+        dateLabel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        dateLabel.setFont(new Font("Arial", Font.PLAIN, 15));
         date = dateFormat.format(Calendar.getInstance().getTime());
-        dateLabel.setText("Its " + date);
+        dateLabel.setText("It's " + date);
 
-        // TODO: tu trzeba jeszcze zrobić
+        today = Calendar.getInstance().get(Calendar.DAY_OF_YEAR);
+        endOfSeason = 169;
+
         daysLeftToEndOfSeasonLabel = new JLabel();
+        daysLeftToEndOfSeasonLabel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
         daysLeftToEndOfSeasonLabel.setFont(new Font("Arial", Font.PLAIN, 15));
-        daysLeftToEndOfSeasonLabel.setText("Season ends in: " + 69 + " days");
+        daysLeftToEndOfSeasonLabel.setText("Season ends in: " + (endOfSeason - today) + " days");
 
-        infoPanel = new JPanel(new GridLayout(6,1,0,0));
-        infoPanel.setPreferredSize(new Dimension(300,200));
-        infoPanel.add(new JLabel(""));
+        infoPanel = new JPanel(new GridLayout(0,1));
+        infoPanel.setPreferredSize(new Dimension(300,100));
         infoPanel.add(welcomeLabel);
         infoPanel.add(descriptionLabel);
         infoPanel.add(dateLabel);
