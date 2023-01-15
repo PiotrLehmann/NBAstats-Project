44e66a58983aec52f802d922e606d8d0cdec72ee
PiotrLehmann <piterrubik@gmail.com>
2023-01-15 12:06:30 +0100

    Divided code into multiple Threads

--- src/Window.java
+++ src/Window.java
@@ -1,17 +1,12 @@
 import org.apache.log4j.Logger;
-
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 
 public class Window extends JFrame {
 
-    // Logger
+    JPanel panelContainer;
     private static final Logger logger = Logger.getLogger(Window.class);
-
-    // To get the resolution of the display (in px)
     Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 
     public Window() {
@@ -33,47 +28,8 @@ public class Window extends JFrame {
         CardLayout cl = new CardLayout();
 
         // Panel Container
-        JPanel panelContainer = new JPanel();
+        panelContainer = new JPanel();
         panelContainer.setLayout(cl);
-
-        // Front Panel with LOGO
-        JPanel homePanel = new JPanel(new GridLayout(1,2));
-        HomeScreen homeScreen = new HomeScreen();
-        homePanel.add(homeScreen.logoLabel);
-        homePanel.add(homeScreen.textLabel);
-        panelContainer.add(homePanel, "Home");
-        logger.info("Home Panel - successfully created");
-
-        // Teams Statistics Panel
-        JPanel teamsRankingPanel = new JPanel(new BorderLayout());
-        TeamStats teamStats = new TeamStats();
-        teamsRankingPanel.add(teamStats.scroll2, BorderLayout.CENTER);
-        panelContainer.add(teamsRankingPanel, "Teams Ranking");
-        logger.info("Teams Ranking Panel - successfully created");
-
-        // Players statistics Panel
-        JPanel playersRankingsPanel = new JPanel(new BorderLayout());
-        PlayerStats playerStats = new PlayerStats();
-        playersRankingsPanel.add(playerStats.scroll, BorderLayout.CENTER);
-        panelContainer.add(playersRankingsPanel, "Players Ranking");
-        logger.info("Players Ranking Panel - successfully created");
-
-        JPanel todayScoresPanel = new JPanel(new BorderLayout());
-        TodayScores todayScores = new TodayScores();
-        todayScoresPanel.add(todayScores.scroll, BorderLayout.CENTER);
-        panelContainer.add(todayScoresPanel, "Today Scores");
-
-        HighestScoresPanel highestScoresPanel = new HighestScoresPanel();
-        HighestScoresData highestScoresData = new HighestScoresData();
-        highestScoresPanel.addHistogramColumn(highestScoresData.data[4][0], Integer.parseInt(highestScoresData.data[4][1]), Color.DARK_GRAY);
-        highestScoresPanel.addHistogramColumn(highestScoresData.data[3][0], Integer.parseInt(highestScoresData.data[3][1]), Color.RED);
-        highestScoresPanel.addHistogramColumn(highestScoresData.data[2][0], Integer.parseInt(highestScoresData.data[2][1]), Color.ORANGE);
-        highestScoresPanel.addHistogramColumn(highestScoresData.data[1][0], Integer.parseInt(highestScoresData.data[1][1]), Color.YELLOW);
-        highestScoresPanel.addHistogramColumn(highestScoresData.data[0][0], Integer.parseInt(highestScoresData.data[0][1]), Color.GREEN);
-        highestScoresPanel.layoutHistogram();
-        panelContainer.add(highestScoresPanel, "Highest Scores");
-
-        cl.show(panelContainer, "Home");
 
         JMenuBar menuBar = new JMenuBar();
 
@@ -101,53 +57,75 @@ public class Window extends JFrame {
         menuBar.add(rankingsMenu);
         menuBar.add(scoresMenu);
 
+        backHomeItem.setEnabled(false);
+        exitItem.setEnabled(false);
+        teamsRankingItem.setEnabled(false);
+        playersRankingItem.setEnabled(false);
+        todayScoresItem.setEnabled(false);
+        highestScoresItem.setEnabled(false);
+
+        HomeScreenThread homeScreenThread = new HomeScreenThread();
+        TeamsRankingThread teamsRankingThread = new TeamsRankingThread();
+        PlayersRankingThread playersRankingThread = new PlayersRankingThread();
+        TodayScoresThread todayScoresThread = new TodayScoresThread();
+        HighestScoresThread highestScoresThread = new HighestScoresThread();
+
+        homeScreenThread.start();
+        teamsRankingThread.start();
+        playersRankingThread.start();
+        todayScoresThread.start();
+        highestScoresThread.start();
+
+        try {
+            homeScreenThread.join();
+            teamsRankingThread.join();
+            playersRankingThread.join();
+            todayScoresThread.join();
+            highestScoresThread.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        backHomeItem.setEnabled(true);
+        exitItem.setEnabled(true);
+        teamsRankingItem.setEnabled(true);
+        playersRankingItem.setEnabled(true);
+        todayScoresItem.setEnabled(true);
+        highestScoresItem.setEnabled(true);
+
+
+
+        cl.show(panelContainer, "Home");
+
         // action listeners to all menu items
-        backHomeItem.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                cl.show(panelContainer, "Home");
-                logger.trace("You are on Home");
-            }
+        backHomeItem.addActionListener(e -> {
+            cl.show(panelContainer, "Home");
+            logger.trace("You are on Home");
         });
 
-        exitItem.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                logger.warn("Initially closed application");
-                System.exit(0);
-            }
+        exitItem.addActionListener(e -> {
+            logger.warn("Initially closed application");
+            System.exit(0);
         });
 
-        teamsRankingItem.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                cl.show(panelContainer, "Teams Ranking");
-                logger.trace("Clicked Teams Ranking - currently on Teams Ranking");
-            }
+        teamsRankingItem.addActionListener(e -> {
+            cl.show(panelContainer, "Teams Ranking");
+            logger.trace("Clicked Teams Ranking - currently on Teams Ranking");
         });
 
-        playersRankingItem.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                cl.show(panelContainer, "Players Ranking");
-                logger.trace("Clicked Players Ranking - currently on Players Ranking");
-            }
+        playersRankingItem.addActionListener(e -> {
+            cl.show(panelContainer, "Players Ranking");
+            logger.trace("Clicked Players Ranking - currently on Players Ranking");
         });
 
-        todayScoresItem.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                cl.show(panelContainer, "Today Scores");
-                logger.trace("Clicked Today Scores - currently on Today Scores");
-            }
+        todayScoresItem.addActionListener(e -> {
+            cl.show(panelContainer, "Today Scores");
+            logger.trace("Clicked Today Scores - currently on Today Scores");
         });
 
-        highestScoresItem.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                cl.show(panelContainer, "Highest Scores");
-                logger.trace("Clicked Today Scores - currently on Highest Scores");
-            }
+        highestScoresItem.addActionListener(e -> {
+            cl.show(panelContainer, "Highest Scores");
+            logger.trace("Clicked Today Scores - currently on Highest Scores");
         });
 
         // Creating the window
@@ -159,6 +137,66 @@ public class Window extends JFrame {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setVisible(true);
         logger.trace("Window created successfully");
+    }
+
+    private class HomeScreenThread extends  Thread {
+        @Override
+        public void run() {
+            JPanel homePanel = new JPanel(new GridLayout(1,2));
+            HomeScreen homeScreen = new HomeScreen();
+            homePanel.add(homeScreen.logoLabel);
+            homePanel.add(homeScreen.textLabel);
+            panelContainer.add(homePanel, "Home");
+            logger.info("Home Panel - successfully created");
+        }
+    }
+
+    private class TeamsRankingThread extends  Thread {
+        @Override
+        public void run() {
+            JPanel teamsRankingPanel = new JPanel(new BorderLayout());
+            TeamStats teamStats = new TeamStats();
+            teamsRankingPanel.add(teamStats.scroll2, BorderLayout.CENTER);
+            panelContainer.add(teamsRankingPanel, "Teams Ranking");
+            logger.info("Teams Ranking Panel - successfully created");
+        }
+    }
+
+    private class PlayersRankingThread extends  Thread {
+        @Override
+        public void run() {
+            JPanel playersRankingsPanel = new JPanel(new BorderLayout());
+            PlayerStats playerStats = new PlayerStats();
+            playersRankingsPanel.add(playerStats.scroll, BorderLayout.CENTER);
+            panelContainer.add(playersRankingsPanel, "Players Ranking");
+            logger.info("Players Ranking Panel - successfully created");
+        }
+    }
+
+    private class TodayScoresThread extends  Thread {
+        @Override
+        public void run() {
+            JPanel todayScoresPanel = new JPanel(new BorderLayout());
+            TodayScores todayScores = new TodayScores();
+            todayScoresPanel.add(todayScores.scroll, BorderLayout.CENTER);
+            panelContainer.add(todayScoresPanel, "Today Scores");
+            logger.info("Today Scores Panel - successfully created");
+        }
+    }
+
+    private class HighestScoresThread extends  Thread {
+        @Override
+        public void run() {
+            HighestScoresPanel highestScoresPanel = new HighestScoresPanel();
+            HighestScoresData highestScoresData = new HighestScoresData();
+            highestScoresPanel.addHistogramColumn(highestScoresData.data[4][0], Integer.parseInt(highestScoresData.data[4][1]), Color.DARK_GRAY);
+            highestScoresPanel.addHistogramColumn(highestScoresData.data[3][0], Integer.parseInt(highestScoresData.data[3][1]), Color.RED);
+            highestScoresPanel.addHistogramColumn(highestScoresData.data[2][0], Integer.parseInt(highestScoresData.data[2][1]), Color.ORANGE);
+            highestScoresPanel.addHistogramColumn(highestScoresData.data[1][0], Integer.parseInt(highestScoresData.data[1][1]), Color.YELLOW);
+            highestScoresPanel.addHistogramColumn(highestScoresData.data[0][0], Integer.parseInt(highestScoresData.data[0][1]), Color.GREEN);
+            highestScoresPanel.layoutHistogram();
+            panelContainer.add(highestScoresPanel, "Highest Scores");
+        }
     }
 
 }
