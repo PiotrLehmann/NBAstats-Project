b3c9311fe94ed00ac34baafe79486cb232890327
PiotrLehmann <piterrubik@gmail.com>
2023-01-15 12:25:06 +0100

    InternetAvailabilityChecker done

--- src/InternetAvailabilityChecker.java
+++ src/InternetAvailabilityChecker.java
@@ -1,9 +1,15 @@
+import org.apache.log4j.Logger;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
+
+
 public class InternetAvailabilityChecker {
+
+    private static final Logger logger = Logger.getLogger(InternetAvailabilityChecker.class);
+
     public static boolean isInternetAvailable() throws IOException {
         return isHostAvailable("google.com") || isHostAvailable("amazon.com")
                 || isHostAvailable("facebook.com")|| isHostAvailable("apple.com");
@@ -14,10 +20,11 @@ public class InternetAvailabilityChecker {
             int port = 80;
             InetSocketAddress socketAddress = new InetSocketAddress(hostName, port);
             socket.connect(socketAddress, 3000);
-
+            logger.info("Internet connection CAN BE established");
             return true;
         }
         catch(UnknownHostException unknownHost) {
+            logger.info("Internet connection CAN NOT BE established");
             return false;
         }
     }
